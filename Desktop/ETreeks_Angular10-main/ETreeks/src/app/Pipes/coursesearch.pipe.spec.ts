9c41eaae8f72ac3a5d2f85fb07d42a7a709456f5
Amer Alief <105926803+ameraleef123@users.noreply.github.com>
2024-08-04 18:05:37 +0300

    Delete Desktop/ETreeks_Angular10-main/ETreeks directory

--- Desktop/ETreeks_Angular10-main/ETreeks/src/app/Pipes/coursesearch.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CoursesearchPipe } from './coursesearch.pipe';
-
-describe('CoursesearchPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CoursesearchPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
